b37a21f2c77a526ab7950437da60c2a6195cfc71
Naim <naim.latifi@hiq.se>
2022-06-06 14:11:28 +0200

    refactored

--- src/components/__tests__/ButtonStyle.test.js
+++ src/components/__tests__/ButtonStyle.test.js
@@ -1,12 +1,18 @@
 import { render, screen, cleanup, fireEvent, act } from '@testing-library/react'
 import ButtonStyle from '../ButtonStyle.js'
 
-test('It should check button on initial render', () => {
+test('It should check button enabled on initial render', () => {
   render(<ButtonStyle />)
   const btn = screen.getByRole('button')
   // check if button is enabled
   expect(btn).toBeEnabled()
 })
+test('button has correct initial text', () => {
+  render(<ButtonStyle />)
+  const btn = screen.getByRole('button', { name: 'Change button to green' })
+  expect(btn).toHaveStyle({ backgroundColor: 'red' })
+})
+
 test('It should check if button is clicked with handler function as prop and only once', () => {
   const mockHandler = jest.fn()
   render(<ButtonStyle handleClick={mockHandler} />)
@@ -15,12 +21,10 @@ test('It should check if button is clicked with handler function as prop and onl
   expect(mockHandler.mock.calls).toHaveLength(1)
 })
 
-test('It should check if button has background color and on button click should change to green', () => {
+test('It should check on button click that background color should change to green', () => {
   const mockHandler = jest.fn()
   render(<ButtonStyle handleClick={mockHandler} />)
   const btn = screen.getByRole('button')
-  expect(btn).toHaveStyle({ backgroundColor: 'red' })
-  expect(btn.textContent).toBe('Change button to green')
   // on button click
   fireEvent.click(btn)
   expect(btn).toHaveStyle({ backgroundColor: 'green' })

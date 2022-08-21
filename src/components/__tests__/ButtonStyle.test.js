090909b775c29841570a6d603616755c72a4e455
Naim <naim.latifi@hiq.se>
2022-08-21 14:15:56 +0200

    more enhancement on unit testing with react testing library

--- src/components/__tests__/ButtonStyle.test.js
+++ src/components/__tests__/ButtonStyle.test.js
@@ -1,32 +1,37 @@
 import { render, screen, cleanup, fireEvent, act } from '@testing-library/react'
 import ButtonStyle from '../ButtonStyle.js'
 
-test('It should check button enabled on initial render', () => {
-  render(<ButtonStyle />)
-  const btn = screen.getByRole('button')
-  // check if button is enabled
-  expect(btn).toBeEnabled()
-})
-test('button has correct initial text', () => {
-  render(<ButtonStyle />)
-  const btn = screen.getByRole('button', { name: 'Change button to green' })
-  expect(btn).toHaveStyle({ backgroundColor: 'red' })
-})
+describe('Test suit for button component', () => {
+  const handleClick = jest.fn()
 
-test('It should check if button is clicked with handler function as prop and only once', () => {
-  const mockHandler = jest.fn()
-  render(<ButtonStyle handleClick={mockHandler} />)
-  const btn = screen.getByRole('button')
-  fireEvent.click(btn)
-  expect(mockHandler.mock.calls).toHaveLength(1)
-})
+  test('It should check button enabled on initial render', () => {
+    render(<ButtonStyle />)
+    const btn = screen.getByRole('button')
+    // check if button is enabled
+    expect(btn).toBeEnabled()
+  })
 
-test('It should check on button click that background color should change to green', () => {
-  const mockHandler = jest.fn()
-  render(<ButtonStyle handleClick={mockHandler} />)
-  const btn = screen.getByRole('button')
-  // on button click
-  fireEvent.click(btn)
-  expect(btn).toHaveStyle({ backgroundColor: 'green' })
-  expect(btn.textContent).toEqual('Change button to red')
+  test('button has correct initial text', () => {
+    render(<ButtonStyle />)
+    const btn = screen.getByRole('button', { name: 'Change button to green' })
+    expect(btn).toHaveStyle({ backgroundColor: 'red' })
+  })
+
+  test('It should check if button is clicked with handler function as prop and only once', () => {
+    render(<ButtonStyle handleClick={handleClick} />)
+    const btn = screen.getByRole('button')
+    fireEvent.click(btn)
+    expect(handleClick).toBeCalledTimes(1)
+    //expect(mockHandler.mock.calls).toHaveLength(1)
+  })
+
+  test('It should check on button click that background color should change to green', () => {
+    render(<ButtonStyle handleClick={handleClick} />)
+    const btn = screen.getByRole('button')
+    // on button click
+    fireEvent.click(btn)
+    expect(btn).toHaveStyle({ backgroundColor: 'green' })
+    expect(btn.textContent).toEqual('Change button to red')
+    expect(handleClick).toBeCalledTimes(1)
+  })
 })
--- src/components/__tests__/Input.test.js
+++ src/components/__tests__/Input.test.js
@@ -5,8 +5,11 @@ describe('Test suit for input', () => {
   it('It should render the input with empty value', () => {
     let { getByRole } = render(<Input value="" />)
     let inputText = getByRole('textbox')
+    // debug element
+    //screen.debug(inputText)
+    const inputValue = 'hej'
     expect(inputText).toHaveValue('')
-    fireEvent.change(inputText, { target: { value: 'hej' } })
-    expect(inputText).toHaveValue('hej')
+    fireEvent.change(inputText, { target: { value: inputValue } })
+    expect(inputText).toHaveValue(inputValue)
   })
 })
